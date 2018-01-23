ae5e326ccd8fe73aeac073dbe9126ba45b8e0aa3
satoshi.a <aoyagi.web@gmail.com>
2018-01-23 16:55:40 +0900

    最初の土台

---
+++ js/my.js
@@ -0,0 +1,11 @@
+//google map
+  $(function() {
+        var map = $('iframe');
+        map.css('pointer-events', 'none');
+        $('#access').click(function() {
+            map.css('pointer-events', 'auto');
+        });
+        map.mouseout(function() {
+            map.css('pointer-events', 'none');
+        })
+    })
